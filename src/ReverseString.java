9cc2c85f7457ab0fec82ee5d2653d7c1a40bc03f
luuNguyen <thanhluu.ssd@gmail.com>
2024-10-30 22:15:21 +0700

    Add 2 points slove

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,16 +1,33 @@
 public class ReverseString {
-    public static String reverseString(String str) {
+    public static String reverseByArr(String str) {
         StringBuilder result = new StringBuilder();
-        char[] strArr = str.toCharArray();
-        for (int i = strArr.length-1; i >0; i--) {
-            result.append(strArr[i]);
+        for (int i = str.length() -1; i >=0; i--) {
+            result.append(str.charAt(i));
         }
         return result.toString();
 
     }
+    
+    public static String reverseBy2Point(String str) {
+        int left=0;
+        int right = str.length() -1;
+        StringBuilder result = new StringBuilder(str);
+//        swap char until left > right
+        while (left < right) {
+            char temp = str.charAt(left);
+            result.setCharAt(left, str.charAt(right));
+            result.setCharAt(right,str.charAt(left));
+            left++;
+            right--;
+            
+        }
+        return result.toString();
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(ReverseString.reverseString("uhsnamiH"));
+        System.out.println(ReverseString.reverseByArr("uhsnamiH"));
+        System.out.println(ReverseString.reverseBy2Point("uhsnamiH"));
     }
 
 }
