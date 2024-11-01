1c765dfc85b2115528065acce018280b926f0e9d
luuNguyen <thanhluu.ssd@gmail.com>
2024-11-01 12:07:27 +0700

    Nerge sorted arr 3 poiter O(m+n)

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
 public class MergeSortedArray {
+//    Good memory, bad runtime
+//    O(mxn)
     public static void merge(int[] nums1, int m, int[] nums2, int n ) {
         if (n == 0) return;
         for (int i = 0; i < nums2.length; i++) {
@@ -23,13 +25,36 @@ public class MergeSortedArray {
 
         }
     }
+//Good runtime, bad memory
+//    O(m+n)
+    public static void merge3Pointer(int[] nums1, int m, int[] nums2, int n ) {
+
+        int p1 = m-1;
+        int p2 = n-1;
+        int p = m+n-1;
+        while (p1 >= 0 && p2 >=0) {
+            if(nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+        while (p2>=0) {
+            nums1[p] = nums2[p2];
+            p--;
+            p2--;
+        }
+    }
 
     public static void main(String[] args) {
-        int[] nums1 = {-1,3,0,0,0,0,0};
-        int[] nums2 = {0,0,1,2,3};
-        int m = 2;
-        int n = 5;
-        MergeSortedArray.merge(nums1,m,nums2,n);
+        int[] nums1 = {0};
+        int[] nums2 = {1};
+        int m = 0;
+        int n = 1;
+        MergeSortedArray.merge3Pointer(nums1,m,nums2,n);
         System.out.println(Arrays.toString(nums1));
     }
 }
