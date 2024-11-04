6941dbd112ce1639df2ff1ec213f4c8cb6f6c4f0
luuNguyen <thanhluu.ssd@gmail.com>
2024-11-04 14:20:40 +0700

    CopressedString

---
+++ src/CompressedString.java
@@ -0,0 +1,23 @@
+public class CompressedString {
+    static String compressedString(String word) {
+        StringBuilder comp = new StringBuilder();
+        int count  = 0;
+        int letf =0, right = 0;
+        while (letf < word.length()) {
+                while (right <word.length() && word.charAt(letf) == word.charAt(right) && count < 9)  {
+                    count++;
+                    right++;
+                }
+            comp.append(count).append(word.charAt(letf));
+            letf=letf+count;
+            count=0;
+        }
+
+        return comp.toString();
+    }
+
+    public static void main(String[] args) {
+        String word = "aaaaaaaaaaaaaabb";
+        System.out.println(compressedString(word));
+    }
+}
