4bb03f993a8eff27378bbe7e18dc5cd5fc09dbb1
luuNguyen <thanhluu.ssd@gmail.com>
2024-11-05 13:46:18 +0700

    Majority Element

---
+++ src/MajorityElement.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class MajorityElement {
+    static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> numFre = new HashMap<Integer, Integer>();
+        int num =nums[0];
+        int refer = 0;
+        for (int j : nums) {
+            if (numFre.containsKey(j)) {
+                numFre.put(j, numFre.get(j) + 1);
+            } else numFre.put(j, 0);
+            if (numFre.get(j) > refer) {
+                refer = numFre.get(j);
+                num = j;
+            }
+        }
+        return num;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,3};
+System.out.println(majorityElement(nums));
+    }
+}
