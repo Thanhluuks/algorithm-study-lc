c8c98508774aa9a01e1d9fc92594b17401053cca
luuNguyen <thanhluu.ssd@gmail.com>
2024-11-18 23:06:11 +0700

    canJump

--- src/ArrString/CanJump.java
+++ src/ArrString/CanJump.java
@@ -11,7 +11,7 @@ public class CanJump {
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,3,1,1,4};
+        int[] nums = {2,100,1,1,4};
         System.out.println(canJump(nums));
     }
 }
