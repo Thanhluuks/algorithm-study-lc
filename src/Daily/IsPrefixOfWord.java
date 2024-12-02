d17d7089be73da0120abf7940b83ed8493df5211
luuNguyen <thanhluu.ssd@gmail.com>
2024-12-02 09:29:15 +0700

    isPrefixOfWord

--- src/Daily/IsPrefixOfWord.java
+++ src/Daily/IsPrefixOfWord.java
@@ -1,4 +1,16 @@
 package Daily;
 
 public class IsPrefixOfWord {
+    static int isPrefixOfWord(String sentence, String searchWord) {
+        String[] word = sentence.split(" ");
+        for (int i = 0; i < word.length; i++) {
+            if (word[i].startsWith(searchWord)) return i+1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String sen = "";
+        System.out.println(isPrefixOfWord(sen, ""));
+    }
 }
