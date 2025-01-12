d5778e2137d7384fdb6e62f61196fa7357a0ad12
luuNguyen <thanhluu.ssd@gmail.com>
2025-01-12 20:17:32 +0700

    CanBeValid

---
+++ src/Daily/CanBeValid.java
@@ -0,0 +1,46 @@
+package Daily;
+
+import java.util.Stack;
+
+public class CanBeValid {
+    static boolean canBeValid(String s, String locked) {
+        int length = s.length();
+
+        // If length of string is odd, return false.
+        if (length % 2 == 1) {
+            return false;
+        }
+
+        Stack<Integer> openBrackets = new Stack<>();
+        Stack<Integer> unlocked = new Stack<>();
+
+        // Iterate through the string to handle '(' and ')'
+        for (int i = 0; i < length; i++) {
+            if (locked.charAt(i) == '0') {
+                unlocked.push(i);
+            } else if (s.charAt(i) == '(') {
+                openBrackets.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!openBrackets.empty()) {
+                    openBrackets.pop();
+                } else if (!unlocked.empty()) {
+                    unlocked.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // Match remaining open brackets with unlocked characters
+        while (
+                !openBrackets.empty() &&
+                        !unlocked.empty() &&
+                        openBrackets.peek() < unlocked.peek()
+        ) {
+            openBrackets.pop();
+            unlocked.pop();
+        }
+
+        return openBrackets.empty();
+    }
+}
